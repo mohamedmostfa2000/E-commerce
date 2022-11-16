8a0141ba231a537552f0f1d96acbc26a44ebf336
mohamed mostfa <mohamed.mostfa1970@gmail.com>
2022-11-16 03:54:31 +0200

    Mohamed Mostfa - creating the login and sign up screens

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,6 @@
-import 'package:e_commerce/shared/custom_txtfield.dart';
+import 'package:e_commerce/pages/register.dart';
+import 'package:e_commerce/shared/colors.dart';
+import 'package:e_commerce/shared/constants.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -6,29 +8,72 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(33.0),
-          child: Column(
-            children: const [
-              SizedBox(
-                height: 64,
-              ),
-              MyTextField(
-                textInputType: TextInputType.emailAddress,
-                isPassword: false,
-                hinttext: "Enter Your Email : ",
-              ),
-              SizedBox(
-                height: 33,
-              ),
-              MyTextField(
-                textInputType: TextInputType.text,
-                isPassword: true,
-                hinttext: "Enter Your Password : ",
-              ),
-            ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 64,
+                ),
+                TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: false,
+                    decoration: decorationTextfield.copyWith(
+                      hintText: "Enter Your Email : ",
+                    )),
+                const SizedBox(
+                  height: 33,
+                ),
+                TextField(
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: decorationTextfield.copyWith(
+                      hintText: "Enter Your Password : ",
+                    )),
+                const SizedBox(
+                  height: 33,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Sign in",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(BTNgreen),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8))),
+                  ),
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Do not have an account?",
+                        style: TextStyle(fontSize: 18)),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Register()),
+                          );
+                        },
+                        child: Text('sign up',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 18))),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
